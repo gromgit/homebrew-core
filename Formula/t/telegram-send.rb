d72ac1f1c8abae21bbb3fdfa288322a4fae2003b
Adrian Ho <the.gromgit@gmail.com>
2025-09-22 10:55:48 +0800

    telegram 0.37 (new formula)

---
+++ Formula/t/telegram-send.rb
@@ -0,0 +1,72 @@
+class TelegramSend < Formula
+  include Language::Python::Virtualenv
+
+  desc "Send messages and files over Telegram from the command-line"
+  homepage "https://github.com/rahiel/telegram-send"
+  url "https://files.pythonhosted.org/packages/79/13/f84085ca81111e54c45105a194844cd79d999801813abdf3674a0cd6086b/telegram-send-0.37.tar.gz"
+  sha256 "9f69abb331598e4b6ba8a173b15df7dfdec4eec17e3600a43d554c53b0150695"
+  license "GPL-3.0-or-later"
+
+  depends_on "python@3.12"
+
+  resource "anyio" do
+    url "https://files.pythonhosted.org/packages/f1/b4/636b3b65173d3ce9a38ef5f0522789614e590dab6a8d505340a4efe4c567/anyio-4.10.0.tar.gz"
+    sha256 "3f3fae35c96039744587aa5b8371e7e8e603c0702999535961dd336026973ba6"
+  end
+
+  resource "appdirs" do
+    url "https://files.pythonhosted.org/packages/d7/d8/05696357e0311f5b5c316d7b95f46c669dd9c15aaeecbb48c7d0aeb88c40/appdirs-1.4.4.tar.gz"
+    sha256 "7d5d0167b2b1ba821647616af46a749d1c653740dd0d2415100fe26e27afdf41"
+  end
+
+  resource "certifi" do
+    url "https://files.pythonhosted.org/packages/dc/67/960ebe6bf230a96cda2e0abcf73af550ec4f090005363542f0765df162e0/certifi-2025.8.3.tar.gz"
+    sha256 "e564105f78ded564e3ae7c923924435e1daa7463faeab5bb932bc53ffae63407"
+  end
+
+  resource "colorama" do
+    url "https://files.pythonhosted.org/packages/d8/53/6f443c9a4a8358a93a6792e2acffb9d9d5cb0a5cfd8802644b7b1c9a02e4/colorama-0.4.6.tar.gz"
+    sha256 "08695f5cb7ed6e0531a20572697297273c47b8cae5a63ffc6d6ed5c201be6e44"
+  end
+
+  resource "h11" do
+    url "https://files.pythonhosted.org/packages/01/ee/02a2c011bdab74c6fb3c75474d40b3052059d95df7e73351460c8588d963/h11-0.16.0.tar.gz"
+    sha256 "4e35b956cf45792e4caa5885e69fba00bdbc6ffafbfa020300e549b208ee5ff1"
+  end
+
+  resource "httpcore" do
+    url "https://files.pythonhosted.org/packages/06/94/82699a10bca87a5556c9c59b5963f2d039dbd239f25bc2a63907a05a14cb/httpcore-1.0.9.tar.gz"
+    sha256 "6e34463af53fd2ab5d807f399a9b45ea31c3dfa2276f15a2c3f00afff6e176e8"
+  end
+
+  resource "httpx" do
+    url "https://files.pythonhosted.org/packages/8c/23/911d93a022979d3ea295f659fbe7edb07b3f4561a477e83b3a6d0e0c914e/httpx-0.25.2.tar.gz"
+    sha256 "8b8fcaa0c8ea7b05edd69a094e63a2094c4efcb48129fb757361bc423c0ad9e8"
+  end
+
+  resource "idna" do
+    url "https://files.pythonhosted.org/packages/f1/70/7703c29685631f5a7590aa73f1f1d3fa9a380e654b86af429e0934a32f7d/idna-3.10.tar.gz"
+    sha256 "12f65c9b470abda6dc35cf8e63cc574b1c52b11df2c86030af0ac09b01b13ea9"
+  end
+
+  resource "python-telegram-bot" do
+    url "https://files.pythonhosted.org/packages/3e/de/fb0ec35cf04c800888ed9eaebbaec771fd0f0ce5694b320e4a233e0432de/python-telegram-bot-20.6.tar.gz"
+    sha256 "c6951dc6b9368d80a17b5d0496ced39bca2c9bafab06cbfdae6226493d8b06fe"
+  end
+
+  resource "sniffio" do
+    url "https://files.pythonhosted.org/packages/a2/87/a6771e1546d97e7e041b6ae58d80074f81b7d5121207425c964ddf5cfdbd/sniffio-1.3.1.tar.gz"
+    sha256 "f4324edc670a0f49750a81b895f35c3adb843cca46f0530f79fc1babb23789dc"
+  end
+
+  def install
+    virtualenv_install_with_resources
+    site_packages = libexec/Language::Python.site_packages("python3@3.12")
+    file = site_packages/"telegram_send/telegram_send.py"
+    inreplace file, "/etc/telegram-send.conf", etc/"telegram-send.conf"
+  end
+
+  test do
+    assert_match "Config not found", shell_output("#{bin}/telegram-send -f #{test_fixtures("test.png")}", 1)
+  end
+end
