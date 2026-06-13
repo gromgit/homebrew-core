f6bfa04feec2911b6eecd13e48c040009a83d91c
Adrian Ho <the.gromgit@gmail.com>
2026-06-13 15:28:37 +0800

    gitpane 0.7.5

---
+++ Formula/g/gitpane.rb
@@ -0,0 +1,26 @@
+class Gitpane < Formula
+  desc "Multi-repo Git workspace dashboard for the terminal"
+  homepage "https://github.com/affromero/gitpane"
+  url "https://github.com/affromero/gitpane/archive/refs/tags/v0.7.5.tar.gz"
+  sha256 "c95cc60ac2e1785b9150e995c71dff9bc42be0a70a34291fb6399bf6ffcd7b93"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test gitpane`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system bin/"program", "do", "something"`.
+    system "false"
+  end
+end
